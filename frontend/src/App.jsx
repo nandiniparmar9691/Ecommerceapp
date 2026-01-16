0a8cb92591e83c16baa54cffccca1abc969905d3
nandiniparmar9691 <nandiniparmar9691@gmail.com>
2026-01-16 23:32:10 +0530

    deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
