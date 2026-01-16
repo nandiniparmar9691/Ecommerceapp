84e22dd0195ab03647de75d7a1adfa0df565933f
nandiniparmar9691 <nandiniparmar9691@gmail.com>
2026-01-16 17:00:51 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
-import Cart from "./pages/cart";
+import Cart from "./pages/Cart";
 import "./App.css";
 
 export default function App() {
   return (
     <BrowserRouter basename="/Ecommerceapp/">
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
